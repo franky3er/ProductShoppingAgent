922646c1a1a2c9df89135955ebee670871649275
franky3er <frankdreyer94@gmail.com>
2017-05-10 16:54:34 +0200

    master - initial commit

---
+++ src/vs/productshoppingagent/main/MainApplication.java
@@ -0,0 +1,10 @@
+package vs.productshoppingagent.main;
+
+/**
+ * Created by franky3er on 10.05.17.
+ */
+public class MainApplication {
+    public static void main(String []args) {
+        //TODO ..........
+    }
+}
